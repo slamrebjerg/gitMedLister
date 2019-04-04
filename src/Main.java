222ee3840259f7976a72d42db2b1476cbd6404ec
slamrebjerg <cph-oh50@cphbusiness.dk>
2019-04-04 12:00:31 +0200

    rettet fejl i main skriv funktion

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     // dettte er main class
 
-    public static void udskriv {
+    public static void udskriv() {
         System.out.println("Dette er Oles funktion");
     }
 
