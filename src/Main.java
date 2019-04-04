2cf97c9ca3f815f1ceb947f92cd004e0579fae8d
Ulrik Holm <ulrikholmuh@gmail.com>
2019-04-04 11:59:09 +0200

    String navn lavet

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     // dettte er main class
+    public static String navn = "Ulrik";
+
     public static void udskriv(){
-        System.out.println("Hej Ulrik");
+        System.out.println("Hej " + navn);
     }
 
 }
