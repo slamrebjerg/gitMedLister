1a24a1cf2f774ebda99f7ea5dacfdb52391e18a4
slamrebjerg <cph-oh50@cphbusiness.dk>
2019-04-04 12:24:36 +0200

    rettet fejl i main

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static  String hej = "hej";
     public static String navn = "Ulrik";
 
-    public static void udskriv(){
+    public static void udskriv3(){
         System.out.println(hej + navn);
     }
 
-    public static void udskriv() {
+    public static void udskriv2() {
         System.out.println("Dette er Oles funktion");
     }
 
